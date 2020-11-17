245dd08be91a99a8073ce9aa32b2c93315bcba32
Benjamin Peronne <aymerick-971@hotmail.fr>
2020-11-17 08:44:34 -0400

    Initial Commit

---
+++ Bash_Build/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Bash_Build
+//
+//  Created by Benjamin Peronne on 17/11/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
